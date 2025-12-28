83afa66cc7dfbf0f7d71d59e34041ff4d0923253
Anas Ali Abd Elmoneim <anas.dev000@gmail.com>
2025-12-28 17:28:27 +0200

    Add allowed origin for Vercel deployment

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ const corsOptions = {
       "http://localhost:5173",
       "http://localhost:3000",
       "http://127.0.0.1:5173",
+      "https://tilal.vercel.app",
       process.env.FRONTEND_URL,
     ].filter(Boolean);
 
